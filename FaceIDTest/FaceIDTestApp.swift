a6a9786906d82c17fdbbde736345d0e8afd9a4a6
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2022-12-11 13:31:16 -0500

    Initial Commit

---
+++ FaceIDTest/FaceIDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDTestApp.swift
+//  FaceIDTest
+//
+//  Created by Daniel Wade Duggin on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
