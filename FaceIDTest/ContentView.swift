992498a9f43dc801ef9210e705691edeb34449f0
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2022-12-11 13:59:06 -0500

    FaceID Implementation

--- FaceIDTest/ContentView.swift
+++ FaceIDTest/ContentView.swift
@@ -5,18 +5,46 @@
 //  Created by Daniel Wade Duggin on 12/11/22.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            if self.isUnlocked{
+                Text("Logged in to CrowdCheck")
+            } else{
+                Text("Please sign in")
+            }
         }
-        .padding()
+        
+        .onAppear(perform: authenticateUser)
     }
+    
+    func authenticateUser(){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let textReason = "We need to access your face"
+            
+            //this enables faceID
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: textReason){success, authenticationError in DispatchQueue.main.async{
+                if success {
+                    self.isUnlocked = true
+                } else{
+                    
+                }
+            }
+            }
+        } else{
+            //error in biometrics
+        }
+    }
+       
 }
 
 struct ContentView_Previews: PreviewProvider {
